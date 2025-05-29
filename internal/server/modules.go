5fc3f84d32a16a2af46a98e4135c5add79f2f54e
Pedro Costa <550684+pnmcosta@users.noreply.github.com>
2025-05-29 09:01:59 +0100

    add network and resource managers to container

--- internal/server/modules.go
+++ internal/server/modules.go
@@ -1,3 +1,20 @@
 package server
 
-// @note this file includes the domain modules, accounts, peers, networks, etc.
+import (
+	"management/internal/modules/networks"
+	"management/internal/modules/networks/manager"
+	"management/internal/modules/networks/resources"
+	resourcesManager "management/internal/modules/networks/resources/manager"
+)
+
+func (s *Server) NetworksManager() networks.Manager {
+	return Create(s, func() networks.Manager {
+		return manager.NewManager(s.Store(), s.Router(), s.PermissionsManager())
+	})
+}
+
+func (s *Server) ResourcesManager() resources.Manager {
+	return Create(s, func() resources.Manager {
+		return resourcesManager.NewManager(s.Store(), s.Router(), s.NetworksManager())
+	})
+}
